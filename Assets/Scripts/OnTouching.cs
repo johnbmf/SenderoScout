d83824cd78b4d645b667c17cfba02c515d705632
Jonathan Mendoza <jonathan.mendoza.14@sansano.usm.cl>
2019-06-01 03:58:22 -0400

    Elementos GUI y fix camara al seleccionar mision

--- Assets/Scripts/OnTouching.cs
+++ Assets/Scripts/OnTouching.cs
@@ -1,23 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OnTouching : MonoBehaviour
 {
     public GameObject DetalleMisionCanvas;
+    public GameObject BotonSonido;
+    public GameObject MainCamera;
+
+    public Sprite SoundOn;
+    public Sprite SoundOff;
+
+    public bool activeSound;
     public bool isInFront = true;
 
     public void OnMouseDown()
     {
-        if (isInFront) {
+        if (isInFront)
+        {
+            MainCamera.GetComponent<TouchCamera>().enabled = false;
             DetalleMisionCanvas.SetActive(!DetalleMisionCanvas.activeSelf);
             isInFront = false;
         }
     }
 
-    public void HideMissionDetails() {
+    public void HideMissionDetails()
+    {
         DetalleMisionCanvas.SetActive(!DetalleMisionCanvas.activeSelf);
         isInFront = true;
+        MainCamera.GetComponent<TouchCamera>().enabled = true;
     }
-        
+
+    public void ToggleMusic()
+    {
+        if (activeSound)
+        {
+            activeSound = !activeSound;
+            BotonSonido.GetComponent<Image>().sprite = SoundOff;
+        }
+
+        else
+        {
+            activeSound = !activeSound;
+            BotonSonido.GetComponent<Image>().sprite = SoundOn;
+        }
+    }
+
 }
