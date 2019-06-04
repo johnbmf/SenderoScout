9385b924dcd500f5f914a30c8daf68601f1e4add
Heimidall <pedroxlda12@gmail.com>
2019-06-04 07:59:28 -0400

    Se agregan los archivos php que realizan la conexión a la base de datos local

---
+++ Menu-dirigente/php/config.php
@@ -0,0 +1,6 @@
+<?php
+$mysqli = mysqli_connect('37.59.55.185','MXgKejhDhB','n39jR2Oqwo','MXgKejhDhB');
+if (!$mysqli) {
+	die('Could not connect to MySQL: ' . mysql_error());
+}
+?>
---
+++ Menu-dirigente/php/crearMision.php
@@ -0,0 +1,66 @@
+<?php include 'config.php';
+
+
+	$json = file_get_contents('php://input');
+
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+
+	 // name store into $tipo_mision.
+	$tipo_mision = $obj['tipo_mision'];
+
+	// same with $nombre_mision.
+	$nombre_mision = $obj['nombre_mision'];
+
+	// same with $descripcion_mision.
+	$descripcion_mision = $obj['descripcion_mision'];
+	$i = 0;
+	$query = "
+	set @unidad = (Select Usuario.unidad1 from Usuario
+		where Usuario.user = 'dirigente1');
+	Select Usuario.user from Usuario where Usuario.unidad1 = @unidad and Usuario.tipo = 'niño';
+	";
+	$usuarios = $mysqli->multi_query($query) or die("Error".mysql_error());
+	$mysqli->next_result();
+	$usuarios2 = $mysqli->store_result();
+	while ($fila = $usuarios2->fetch_array()){
+
+		$users[$i] = $fila[0];
+		$i++;
+		// code...
+	}
+
+	if($obj['nombre_mision']!="")
+	{
+
+	$result= $mysqli->query("SELECT * FROM mision where nombre_mision='$nombre_mision'");
+
+
+		if($result->num_rows>0){
+			echo json_encode('Ya existe una mision con ese nombre.');  // alert msg in react native
+		}
+		else
+		{
+				$add = $mysqli->query("insert into mision (tipo_mision,nombre_mision,descripcion_mision) values('$tipo_mision','$nombre_mision','$descripcion_mision')");
+
+			if($add){
+				$query_id_mision = $mysqli->query("SELECT id_mision from mision where nombre_mision = '$nombre_mision'");
+					$id_mision = $query_id_mision->fetch_array();
+					foreach ($users as $key => $value) {
+						$add_mision_mapa = $mysqli->query("insert into mision_mapa (user,estado,puntaje,fecha_expiracion,fecha_revision,id_mision) values('$value',0,0,DATE_ADD(CURRENT_DATE, INTERVAL 7 DAY),NULL,$id_mision[0])") or die("Error en los insert".mysql_error());
+				}
+				echo  json_encode('Mision creada con exito'); // alert msg in react native
+			}
+			else{
+			   echo json_encode('Compruebe su conexión a internet.'); // our query fail
+			}
+
+		}
+	}
+
+	else{
+	  echo json_encode('Intente de nuevo');
+	}
+  mysqli_close($mysqli);
+
+?>
