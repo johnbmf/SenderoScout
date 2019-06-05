6abf7bda93a27d6c2057b2e375d73c14a7b86b36
Ianmp <Ianmp@users.noreply.github.com>
2019-06-05 16:56:53 -0400

    arreglos de testeo

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -62,8 +62,11 @@ public class Login : MonoBehaviour{
             SceneManager.UnloadSceneAsync("LoginMenu");
         }   
         else{
-            char[] toTrim1 = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':' };
-            errorIngreso.text = www.text.Trim(toTrim1); 
+            char[] toTrim1 = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '-'};
+            errorIngreso.text = www.text.Trim(toTrim1);
+            nameField.text = "";
+            passwordField.text = "";
+
             Debug.Log("falló el ingreso. Error # " + www.text);
         }
 
