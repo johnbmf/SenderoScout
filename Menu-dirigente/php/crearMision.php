9de544101e1285a10dbc5c6debdbe28b4a9ae93e
Heimidall <pedroxlda12@gmail.com>
2019-08-31 05:52:17 -0400

    Se añade opcion para seleccionar spot y para asignar fecha de expiracion a una mision, se realizan los cambios al script php para mantener consistencia

--- Menu-dirigente/php/crearMision.php
+++ Menu-dirigente/php/crearMision.php
@@ -1,71 +1,57 @@
 <?php
 	require_once('db_conn.php');
   //Poner una app verification con post.
-
   //Generar conexion a BD.
     $mysqli = db_conn();
-
-
 	$json = file_get_contents('php://input');
-
 	 // decoding the received JSON and store into $obj variable.
 	 $obj = json_decode($json,true);
-
 	 // name store into $tipo_mision.
 	$tipo_mision = $obj['tipo_mision'];
-
 	// same with $nombre_mision.
 	$nombre_mision = $obj['nombre_mision'];
-
+	$spot = $obj['spot'];
+	$expiracion = $obj['expiracion'];
 	// same with $descripcion_mision.
 	$descripcion_mision = $obj['descripcion_mision'];
 	$i = 0;
 	$query = "
 	set @unidad = (Select Usuario.unidad1 from Usuario
-		where Usuario.user = 'dirigente1');
-	Select Usuario.user from Usuario where Usuario.unidad1 = @unidad and Usuario.tipo = 'niño';
+		where Usuario.user = 'dirigente');
+	Select Usuario.user from Usuario where Usuario.unidad1 = @unidad and Usuario.tipo = 'nino';
 	";
 	$usuarios = $mysqli->multi_query($query) or die("Error".mysql_error());
 	$mysqli->next_result();
 	$usuarios2 = $mysqli->store_result();
 	while ($fila = $usuarios2->fetch_array()){
-
 		$users[$i] = $fila[0];
 		$i++;
 		// code...
 	}
-
 	if($obj['nombre_mision']!="")
 	{
-
 	$result= $mysqli->query("SELECT * FROM mision where nombre_mision='$nombre_mision'");
-
-
 		if($result->num_rows>0){
 			echo json_encode('Ya existe una mision con ese nombre.');  // alert msg in react native
 		}
 		else
 		{
 				$add = $mysqli->query("insert into mision (tipo_mision,nombre_mision,descripcion_mision) values('$tipo_mision','$nombre_mision','$descripcion_mision')");
-
 			if($add){
 				$query_id_mision = $mysqli->query("SELECT id_mision from mision where nombre_mision = '$nombre_mision'");
 					$id_mision = $query_id_mision->fetch_array();
 					foreach ($users as $key => $value) {
-						$add_mision_mapa = $mysqli->query("insert into mision_mapa (user,estado,puntaje,fecha_expiracion,fecha_revision,id_mision) values('$value',0,0,DATE_ADD(CURRENT_DATE, INTERVAL 7 DAY),NULL,$id_mision[0])") or die("Error en los insert".mysql_error());
+						$add_mision_mapa = $mysqli->query("insert into mision_mapa (user,estado,puntaje,fecha_expiracion,fecha_borrado,spot,id_mision) values('$value',0,0,DATE_ADD(CURRENT_DATE, INTERVAL $expiracion DAY),DATE_ADD(CURRENT_DATE, INTERVAL ($expiracion+2) DAY),'$spot',$id_mision[0])")or die("Error en los insert".mysql_error());
 				}
 				echo  json_encode('Mision creada con exito'); // alert msg in react native
 			}
 			else{
-			   echo json_encode('Compruebe su conexión a internet.'); // our query fail
+			   echo json_encode('Compruebe su conexion a internet.'); // our query fail
 			}
-
 		}
 	}
-
 	else{
 	  echo json_encode('Intente de nuevo');
 	}
   mysqli_close($mysqli);
-
 ?>
--- Menu-dirigente/screens/crear_mision.js
+++ Menu-dirigente/screens/crear_mision.js
@@ -9,10 +9,11 @@ import {
     TouchableWithoutFeedback,
     Keyboard,
     Alert,
+    TouchableOpacity,
     KeyboardAvoidingView,
     ScrollView
 } from "react-native";
-import { Header,Left,Right,Icon} from 'native-base'
+import {Header,Left,Right,Icon, Body} from 'native-base'
 const DimissKeyboard = ({children}) => (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
         {children}
@@ -22,9 +23,11 @@ class crear_mision extends Component {
     constructor(props){
         super(props);
         this.state = {
-            PickerValue : 0,
+            TipoMision : 0,
             nombre_mision : '',
-            desc_mision: ''
+            desc_mision: '',
+            Spot: '',
+            Expiracion: 0
 
         }
     }
@@ -36,11 +39,17 @@ class crear_mision extends Component {
     }
     crearMision = () =>{
         console.log(this.state);
-        if(this.state.PickerValue==0){
+        if(this.state.TipoMision==0){
             Alert.alert("Error","Es necesario elegir el tipo de misión");
             return;
         }
-        else if (!this.state.desc_mision || !this.state.desc_mision.trim()) {
+        else if (!this.state.Spot || !this.state.Spot.trim()) {
+            Alert.alert("Error","Es necesario asignar una ubicación a la misión");
+            return;
+        }else if (this.state.Expiracion==0) {
+            Alert.alert("Error","Es necesario asignar un tiempo de expiración de la misión");
+            return;
+        }else if (!this.state.desc_mision || !this.state.desc_mision.trim()) {
             Alert.alert("Error","Es necesaria una descripción de la misión");
             return;
         }else if (!this.state.nombre_mision || !this.state.nombre_mision.trim()) {
@@ -56,9 +65,11 @@ class crear_mision extends Component {
                     
                 },
                 body:JSON.stringify({
-                    tipo_mision:this.state.PickerValue,
-                    nombre_mision:this.state.nombre_mision,
-                    descripcion_mision:this.state.desc_mision
+                    "tipo_mision":this.state.TipoMision,
+                    "nombre_mision":this.state.nombre_mision,
+                    "descripcion_mision":this.state.desc_mision,
+                    "spot":this.state.Spot,
+                    "expiracion":this.state.Expiracion
                 })
             })
             .then(response => response.json())
@@ -75,14 +86,101 @@ class crear_mision extends Component {
             
             return (
                 <KeyboardAvoidingView style = {{flex:1}} behavior = "padding">
-                    <ScrollView> 
-                        <DimissKeyboard>
-                            <View style = {styles.container}>
-                                <View style={{width: '100%', height: '25%', backgroundColor: 'powderblue'}} />
-                            </View>
-                        </DimissKeyboard>
-                    </ScrollView>
+                <ScrollView contentContainerStyle={{ flexGrow: 1 }}> 
+                <View style = {styles.container}>
+                    <View style={{width: '100%', height: '12%', alignItems:'center'}} > 
+                        
+                        <Header style={{width: '100%', height: '100%',backgroundColor: '#81C14B',font:'Roboto'}}>
+                            <Left>
+                                <Icon name="menu" style = {{paddingTop:20}} onPress = {()=> this.props.navigation.openDrawer()}/>
+                            </Left>
+                            <Body style = {{justifyContent:'center'}}> 
+                                <Text style= {styles.banner} onPress = {()=> this.props.navigation.openDrawer()}> Sendero Scout</Text>
+                            </Body>
+                        </Header >                    
+                    </View>
+
+                    <View style={{width: '100%', height: '7%'}} >
+                        <View style = {styles.top}>
+                            <Text style = {styles.header} >C R E A R  M I S I O N</Text>
+                        </View>
+                    </View>
+                    <View style={{width: '100%', height: '7%'}} > 
+                        <View style= {styles.pickerMenu}>
+                            <Picker 
+                                style = {{width:'70%', borderColor:'gray', borderWidth:1,alignSelf: 'center',flexDirection: 'row'}}
+                                mode = 'dropdown'
+                                selectedValue = {this.state.TipoMision}
+                                onValueChange ={ (itemValue,itemIndex) => this.setState({TipoMision: itemValue}) }>
+                                <Picker.Item label = "Elija un tipo de misión" value = {0} />
+                                <Picker.Item label = "Tipo misión: Pregunta Abierta" value = {1} />
+                            </Picker>
+                        </View>
+                    </View>
+                    <View style={{width: '100%', height: '7%'}} > 
+                        <View style= {styles.pickerMenu}>
+                            <Picker 
+                                style = {{width:'70%', borderColor:'gray', borderWidth:1,alignItems: 'center',flexDirection: 'row'}}
+                                mode = 'dropdown'
+                                selectedValue = {this.state.Spot}
+                                onValueChange ={ (itemValue,itemIndex) => this.setState({Spot: itemValue}) }>
+                                <Picker.Item label = "Elija la ubicación de la misión" value = {0} />
+                                <Picker.Item label = "Campamento" value = {'campamento'} />
+                                <Picker.Item label = "Caverna" value = {'caverna'} />
+                                <Picker.Item label = "Bosque" value = {'bosque'} />
+                            </Picker>
+                        </View>
+                    </View>
+                    <View style={{width: '100%', height: '7%'}} > 
+                        <View style= {styles.pickerMenu}>
+                            <Picker 
+                                style = {{width:'70%', borderColor:'gray', borderWidth:1,alignItems: 'center',flexDirection: 'row'}}
+                                mode = 'dropdown'
+                                selectedValue = {this.state.Expiracion}
+                                onValueChange ={ (itemValue,itemIndex) => this.setState({Expiracion: itemValue}) }>
+                                <Picker.Item label = "Elija la duración de la misión" value = {0} />
+                                <Picker.Item label = "3 días" value = {3} />
+                                <Picker.Item label = "7 dias" value = {7} />
+                                <Picker.Item label = "14 días" value = {14} />
+                            </Picker>
+                        </View>
+                    </View>
+                    <View style={{width: '100%', height: '7%'}} >
+                                <TextInput 
+                                    style = {{height:'100%', width:'95%', borderColor: 'gray', borderWidth:1, textAlign:'center', justifyContent:'center',alignSelf:'center'}}
+                                    underlineColorAndroid = "transparent"
+                                    maxLength = {60}
+                                    //{...this.props}
+                                    multiline = {true}
+                                    numberOfLines = {4}
+                                    onChangeText={(valor) => this.setState({nombre_mision : valor})}
+                                    placeholder = "Nombre de la misión"
+                                    value={this.state.nombre_mision}
+                                    />
+                    </View>
+                    <View style={{width: '100%', height: '40%'}} >
+                                <TextInput 
+                                    style = {{height:'100%', width:'95%', borderColor: 'gray', borderWidth:1, textAlign:'center', justifyContent:'center',alignSelf:'center'}}
+                                    placeholder = "Descripción de la misión"
+                                    maxLength = {240}
+                                    //{...this.props}
+                                    multiline = {true}
+                                    numberOfLines = {20}
+                                    onChangeText={(valor) => this.setState({desc_mision : valor})}
+                                    value={this.state.desc_mision}
+                                    />
+                    </View>
+                    <View style={{width: '100%', height: '8%',alignItems:'center', justifyContent:'center'}} >
+                        <TouchableOpacity 
+                        onPress = {this.crearMision}
+                        style = {{flex:1,width:'40%', height:'100%', backgroundColor: '#104F55', justifyContent:'center'}}>
+                            <Text style = {{color: 'white', textAlign:'center', fontSize:18, backgroundColor: '#104F55'}}> Crear </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                </ScrollView>
                 </KeyboardAvoidingView>
+
         );
     }
 }
@@ -92,6 +190,58 @@ export default crear_mision;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width:'100%',
+        height:'100%'
+    },
+    banner:{
+        color:'white',
+        fontSize:28,
+        justifyContent:'center', 
+        alignItems:'center',
+        alignContent:'center',
+        fontFamily:'Roboto',
+        paddingTop:20
+    },
+    top:{
+        flex:1,
+        flexDirection: 'column',
+        height:'100%',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header:{
+        flex: 1,
+        color: 'black',
+        fontSize: 28,
+        borderColor: 'black',
+        justifyContent:'center', 
+        alignItems:'center'
+    },
+    pickerMenu: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        width: '100%',
+        height: '100%'
+    },
+    misionInput:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width:'100%',
+        height:'100%'
+
+    },    
+    misionDesc:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        //backgroundColor: 'white'   //'#F4F0BB'
+
+
+
     }
 });
