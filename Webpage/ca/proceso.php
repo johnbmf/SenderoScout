13bb696636fc83b8a963ab5c04062dab577d24cd
claudoblei <claudoblei@live.cl>
2019-08-30 23:52:12 -0400

    cambio

--- Webpage/ca/proceso.php
+++ Webpage/ca/proceso.php
@@ -1,23 +1,22 @@
 <?php
 
-$para = 'angelmendozav@gmail.com';
+$para = 'hammersoft.fsw@gmail.com';
 $titulo = 'Contacto a través de formulario web';
-$nombre = $_POST["Nombre"];
-$empresa = $_POST["Empresa"];
-$telefono = $_POST["Telefono"];
-$email = $_POST["E-mail"];
-$mensaje = $_POST["Mensaje"];
+$nombre = $_POST["sender"];
+$email = $_POST["senderEmail"];
+$mensaje = $_POST["message"];
 
 $mensaje = wordwrap($mensaje, 70, "\r\n");
-$enviar = 'Nombre: '. $nombre . "\r\n" . 'Telefono: '. $telefono . "\r\n" . 'Email: '. $email . "\r\n" . 'Mensaje: '. $mensaje;
+$enviar = 'Nombre: '. $nombre . "\r\n" . 'Email: '. $email . "\r\n" . 'Mensaje: '. $mensaje;
 
 $cabeceras = 'From: ' . $email . "\r\n" .
-    'Name: '. $nombre . "\r\n" .
-	'Telefono: '. $telefono . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-	
+     'Name: '. $nombre . "\r\n" .
+	   'Telefono: '. $telefono . "\r\n" .
+     'X-Mailer: PHP/' . phpversion();
+
 if (mail($para, $titulo, $enviar, $cabeceras)){
-    header('Location: http://www.mitra.cl');
+    //header('Location: RUTA');
+    echo "Ok";
 }
 else{
     echo "Error.";
