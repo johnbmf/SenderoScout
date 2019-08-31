153c91ad019021556c8c52186dd129192407c489
claudoblei <claudoblei@live.cl>
2019-08-30 23:17:09 -0400

    saasd

--- Webpage/Tipo1/contact.php
+++ Webpage/Tipo1/contact.php
@@ -1,28 +1,9 @@
 
 <?php
 
-if (isset($_POST["submit"])) {
-	require 'phpmailer/PHPMailerAutoload.php';
-	function sendemail($to, $from, $fromName, $body){
-		$mail = new phpMailer();
-		$mail ->setForm($from, $fromName);
-		$mail ->addAdress($to);
-		$mail ->Subject ='Contact Form - Email';
-		$mail ->Body = $body;
-		$mail -> isHTML (isHtml: false);
+if (isset($_POST["submit"]) {
 
-		return $mail->send();
 	}
 
-	$name = $POST["name"];
-	$email = $POST["email"];
-	$body = $POST["message"];
-
-
-    if (sendemail(to:hammersoft.fesw@gmail.com,$email,$name,$body){
-    	$msg="<p>Thank you! Your message has been sent.</p>";
-    	}else{ $msg = "Email Failed"
-    	}
-}
-
+)
 ?>
