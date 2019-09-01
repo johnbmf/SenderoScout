9bbdbc854f1467bb88492b87f966a75d8779a0e8
diegomontecinos <diegomontecinos@users.noreply.github.com>
2019-09-01 10:35:19 -0400

    Insert a la base de datos funcionando
    se creo el script php Evaluar aptitudes
    se resolvieron problemas de interfaz
    la vista de evaluar aptitudes quedo funcionando

--- Menu-dirigente/php/EvaluarAptitudes.php
+++ Menu-dirigente/php/EvaluarAptitudes.php
@@ -1,7 +1,28 @@
 <?php
 	require_once('db_conn.php');
-  //Generar conexion a BD.
-    $mysqli = db_conn();
+  $mysqli = db_conn(); //generar conexion bd
 
+	$json = file_get_contents('php://input'); // recivir Json desde React
+  $obj = json_decode($json,true); //convertir json en objeto
+  $fecha = new DateTime();
+  //SETEAR VARIABLES DESDE JSON
+  $usuario = $obj['usuario'];
+  $nombre = $obj['nombre'];
+  $fecha = date("Y-m-d H:i:s");
+	$corporalidad = $obj['corporalidad'];
+  $creatividad = $obj['creatividad'];
+  $caracter =$obj['caracter'];
+  $afectividad =$obj['afectividad'];
+  $sociabilidad =$obj['sociabilidad'];
+  $espiritualidad =$obj['espiritualidad'];
+
+
+	$add = $mysqli->query("INSERT INTO evalaptitud (fecha, usuario, nombre, corporalidad, creatividad, caracter, afectividad, sociabilidad, espiritualidad) values('$fecha','$usuario','$nombre', '$corporalidad', '$creatividad', '$caracter','$afectividad','$sociabilidad','$espiritualidad')");
+	if($add){
+		echo  json_encode('Evaluacion enviada con exito'); // alert msg in react native
+	}
+	else{
+		echo json_encode('Compruebe su conexion a internet.'); // our query fail
+	  }
   mysqli_close($mysqli);
 ?>
--- Menu-dirigente/screens/evaluacion_aptitudes.js
+++ Menu-dirigente/screens/evaluacion_aptitudes.js
@@ -5,17 +5,17 @@ import {
     StyleSheet,
     Picker,
 } from "react-native";
-import { Header,Left,Body} from 'native-base'
-import {Icon,Rating, Button } from 'react-native-elements'
-//import Icon from 'react-native-vector-icons/FontAwesome';
+import { Icon,Header,Left,Body} from 'native-base'
+import {Rating, Button } from 'react-native-elements'
 const WOLF_HEAD = require('../assets/Wolf_Head.png')
+const DEFAULT_RATING = 2
 
 class evalaptitudes extends Component {
     
     static navigationOptions = {
         drawerLabel: 'Consejo de la Tarde',
         drawerIcon: ({tintColor}) => (
-            <Icon name='cannabis' style = {{fontSize:24,color:tintColor}} />
+            <Icon name='angellist' type = 'FontAwesome' style = {{fontSize:24,color:tintColor}} />
         )
     }
 
@@ -23,12 +23,18 @@ class evalaptitudes extends Component {
         super(props)
         this.state = {
             isLoading: true,
-            PickerValue: "",
+            PickerValue: 'default',
             dataSource: [],
+            users:[],
+            nombres: [],
+            corporalidad: DEFAULT_RATING,
+            creatividad: DEFAULT_RATING,
+            caracter: DEFAULT_RATING,
+            afectividad: DEFAULT_RATING,
+            sociabilidad:DEFAULT_RATING,
+            espiritualidad:DEFAULT_RATING
         };
     }
-
-
     componentDidMount(){
         fetch('http://192.168.50.65/SS/GetNinosSeisena.php',{
             method: 'POST',
@@ -48,10 +54,52 @@ class evalaptitudes extends Component {
             })   
         }).catch((error) => {
             console.error(error);
-        }); 
+        });
+
+    }
+
+
+    EnviarEvaluacion = () =>{
+
+        if(this.state.PickerValue == 'default'){
+            Alert.alert("Error","Es necesario elegir a un lobato");
+            return;
+        }
+        else{
+            
+            fetch('http://192.168.50.65/SS/EvaluarAptitudes.php',{
+                method: 'POST',
+                header:{
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    
+                },
+                body:JSON.stringify({
+                    "usuario":this.state.users[this.state.nombres.indexOf(this.state.PickerValue)],
+                    "nombre":this.state.PickerValue,
+                    "corporalidad":this.state.corporalidad,
+                    "creatividad":this.state.creatividad,
+                    "caracter":this.state.caracter,
+                    "afectividad":this.state.afectividad,
+                    "sociabilidad":this.state.sociabilidad,
+                    "espiritualidad":this.state.espiritualidad
+                })
+            })
+
+        }
     }
    
     render() {
+        //let nombres = this.state.dataSource.map(({nombre, user}) => ({nombre,user}));
+
+        if(this.state.nombres.length < 1){
+            this.state.nombres = this.state.dataSource.map(({nombre}) => nombre);
+            this.state.users = this.state.dataSource.map(({user}) => user);
+        };
+        console.log(this.state.nombres)
+
+
+
         return (
             <View style={styles.container}>   
                 <View style={{width: '100%', height: '12%', alignItems:'center'}} >     
@@ -69,17 +117,11 @@ class evalaptitudes extends Component {
                     <View syle ={styles.Picker}>
                         <Picker 
                             selectedValue = {this.state.PickerValue}
-                            style = {{width: '100%', height: '100%', borderColor:'gray', borderWidth:1}}
-                            onValueChange = {(itemValue, itemIndex) => this.setState({PickerValue : itemValue})
-                            
-                            }>
-                            <Picker.Item label = 'Seleccione area de desarrollo' value = {null}/>   
-                            <Picker.Item label = 'Corporalidad' value = {'corporalidad'}/>
-                            <Picker.Item label = 'Creatividad' value = {'creatividad'} />
-                            <Picker.Item label = 'Carácter' value = {'caracter'} />
-                            <Picker.Item label = 'Afectividad' value = {'afectividad'} />
-                            <Picker.Item label = 'Sociabilidad' value = {'sociabilidad'} />
-                            <Picker.Item label = 'Espiritualidad' value = {'espiritualidad'} />
+                            style = {{width: '80%', height: '100%', borderColor:'gray', borderWidth:1}}
+                            onValueChange = {(itemValue, itemIndex) => this.setState({PickerValue : itemValue})}>
+                            <Picker.Item label = 'Seleccione un Lobato' value = {'default'}/>
+                            {this.state.nombres.map((item, index) => {return (<Picker.Item label={item} value={item} key={index}/>)})}
+
                         </Picker>
                     </View>
                 </View>
@@ -100,10 +142,11 @@ class evalaptitudes extends Component {
                                         ratingImage={WOLF_HEAD}
                                         ratingColor='#f7ec1e'
                                         ratingBackgroundColor='#c8c7c8'
+                                        startingValue = {DEFAULT_RATING}
                                         ratingCount={5}
                                         imageSize={35}
-                                        onFinishRating={this.ratingCompleted}
                                         style={{ paddingVertical: 10 }}
+                                        onFinishRating={(valor) => this.setState({corporalidad : valor})}
                                     />
                                 </View>
                             </View>  
@@ -122,10 +165,11 @@ class evalaptitudes extends Component {
                                         ratingImage={WOLF_HEAD}
                                         ratingColor='#f7ec1e'
                                         ratingBackgroundColor='#c8c7c8'
+                                        startingValue = {DEFAULT_RATING}
                                         ratingCount={5}
                                         imageSize={35}
-                                        onFinishRating={this.ratingCompleted}
                                         style={{ paddingVertical: 10 }}
+                                        onFinishRating={(valor) => this.setState({creatividad : valor})}
                                     />
                                 </View>
                             </View>  
@@ -144,10 +188,11 @@ class evalaptitudes extends Component {
                                         ratingImage={WOLF_HEAD}
                                         ratingColor='#f7ec1e'
                                         ratingBackgroundColor='#c8c7c8'
+                                        startingValue = {DEFAULT_RATING}
                                         ratingCount={5}
                                         imageSize={35}
-                                        onFinishRating={this.ratingCompleted}
                                         style={{ paddingVertical: 10 }}
+                                        onFinishRating={(valor) => this.setState({caracter : valor})}
                                     />
                                 </View>
                             </View>  
@@ -166,10 +211,11 @@ class evalaptitudes extends Component {
                                         ratingImage={WOLF_HEAD}
                                         ratingColor='#f7ec1e'
                                         ratingBackgroundColor='#c8c7c8'
+                                        startingValue = {DEFAULT_RATING}
                                         ratingCount={5}
                                         imageSize={35}
-                                        onFinishRating={this.ratingCompleted}
                                         style={{ paddingVertical: 10 }}
+                                        onFinishRating={(valor) => this.setState({afectividad : valor})}
                                     />
                                 </View>
                             </View>  
@@ -188,10 +234,11 @@ class evalaptitudes extends Component {
                                         ratingImage={WOLF_HEAD}
                                         ratingColor='#f7ec1e'
                                         ratingBackgroundColor='#c8c7c8'
+                                        startingValue = {DEFAULT_RATING}
                                         ratingCount={5}
                                         imageSize={35}
-                                        onFinishRating={this.ratingCompleted}
                                         style={{ paddingVertical: 10 }}
+                                        onFinishRating={(valor) => this.setState({sociabilidad : valor})}
                                     />
                                 </View>
                             </View>  
@@ -210,10 +257,11 @@ class evalaptitudes extends Component {
                                         ratingImage={WOLF_HEAD}
                                         ratingColor='#f7ec1e'
                                         ratingBackgroundColor='#c8c7c8'
+                                        startingValue = {DEFAULT_RATING}
                                         ratingCount={5}
                                         imageSize={35}
-                                        onFinishRating={this.ratingCompleted}
                                         style={{ paddingVertical: 10 }}
+                                        onFinishRating={(valor) => this.setState({espiritualidad : valor})}
                                     />
                                 </View>
                             </View>  
@@ -222,16 +270,19 @@ class evalaptitudes extends Component {
                 </View>
 
                 <View style ={ {width: '100%', height: '35%' }}>
-                    <View style={{width: '100%', height: '15%', alignItems:'center', justifyContent:'center'}} >
+                    <View style = {{height: '30%'}}>
+
+                    </View>
+                    <View style={{height: '70%', alignItems:'center', justifyContent:'center'}} >
                         <Button
-                        title = "Evaluar"
+                        onPress = {this.EnviarEvaluacion}
                         icon = {
                             <Icon
-                            name = 'check'
-                            size = '15'
-                            color = 'White'
+                            name= 'send'
+                            type= 'Feather'
                             />
-                        }
+                        }iconRight
+                        title = "Evaluar   "
                         />
                     </View>
 
