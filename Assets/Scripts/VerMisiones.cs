9d1414406a2e8f423db26548bd2db7668803720b
Ianmp <Ianmp@users.noreply.github.com>
2019-09-04 00:03:02 -0400

    cambio panel Ver Misiones

--- Assets/Scripts/VerMisiones.cs
+++ Assets/Scripts/VerMisiones.cs
@@ -59,6 +59,11 @@ public class VerMisiones : MonoBehaviour
         canvasMision.SetActive(true);
     }
 
+    public void CerrarCanvas()
+    {
+        canvasMision.SetActive(false);
+    }
+
     IEnumerator Misiones()
     {
         WWWForm form = new WWWForm();
