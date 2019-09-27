1de7497489e0ce8afcb46d1ac3fe643b32ff5ca4
claudia <claudia.hazard.14@sansano.usm.cl>
2019-09-27 01:05:38 -0300

    Crear_unidad inicio

--- Menu-dirigente/screens/crear_unidad.js
+++ Menu-dirigente/screens/crear_unidad.js
@@ -2,9 +2,22 @@ import React, { Component } from "react";
 import { 
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Picker,
+    TextInput,
+    Button,
+    Modal,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Alert,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    ScrollView,
+    ActivityIndicator
 } from "react-native";
-import { Header,Left,Right,Icon } from 'native-base'
+import { Header,Left,Right,Icon,Body } from 'native-base'
+import {SCLAlert, SCLAlertButton} from 'react-native-scl-alert'
+import { NavigationEvents } from 'react-navigation';
 class crear_unidad extends Component {
     static navigationOptions = {
         drawerLabel: 'Crear Unidad',
@@ -12,12 +25,48 @@ class crear_unidad extends Component {
             <Icon name='person-add' style = {{fontSize:24,color:tintColor}} />
         )
     }
+   state = {
+      email: '',
+      password: ''
+   }
+   handleEmail = (text) => {
+      this.setState({ email: text })
+   }
+   handlePassword = (text) => {
+      this.setState({ password: text })
+   }
+   login = (email, pass) => {
+      alert('email: ' + email + ' password: ' + pass)
+   }
     render() {
-        return (
-            <View style={styles.container}>
-                <Text>crear_unidad</Text>
-            </View>
-        );
+    return (
+                <View style = {styles.container}>
+                    <View style={{width: '100%', height: '12%', alignItems:'center'}} > 
+                        
+                        <Header style={{width: '100%', height: '100%',backgroundColor: '#81C14B',font:'Roboto'}}>
+                            <Left>
+                                <Icon name="menu" style = {{paddingTop:20}} onPress = {()=> this.props.navigation.openDrawer()}/>
+                            </Left>
+                            <Body style = {{justifyContent:'center'}}> 
+                                <Text style= {styles.banner} onPress = {()=> this.props.navigation.openDrawer()}>Crear Unidad</Text>
+                            </Body>
+                        </Header > 
+                    </View>
+                        <View style={{width: '100%', height: '7%'}} >
+                                <TextInput 
+                                    style = {{height:'100%', width:'90%', borderColor: 'gray', borderWidth:1, textAlign:'center', justifyContent:'center',alignSelf:'center'}}
+                                    underlineColorAndroid = "transparent"
+                                    maxLength = {60}
+                                    //{...this.props}
+                                    multiline = {true}
+                                    numberOfLines = {4}
+                                    onChangeText={(valor) => this.setState({nombre_unidad : valor})}
+                                    placeholder = "Ingrese nombre de la unidad"
+                                    value={this.state.nombre_unidad}
+                                    />
+                    </View>
+                </View>
+    );
     }
 }
 export default crear_unidad;
@@ -25,7 +74,58 @@ export default crear_unidad;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width:'100%',
+        height:'100%'
+    },
+    banner:{
+        color:'white',
+        fontSize:28,
+        justifyContent:'center', 
+        alignItems:'center',
+        alignContent:'center',
+        fontFamily:'Roboto',
+        paddingTop:20
+    },
+    top:{
+        flex:1,
+        flexDirection: 'column',
+        height:'100%',
+        width: '100%',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+    },
+    header:{
+        flex: 1,
+        color: 'black',
+        fontSize: 28,
+        borderColor: 'black',
+        justifyContent:'center', 
+        alignItems:'center'
+    },
+    pickerMenu: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        width: '100%',
+        height: '100%'
+    },
+    misionInput:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width:'100%',
+        height:'100%'
+
+    },    
+    misionDesc:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        //backgroundColor: 'white'   //'#F4F0BB'
+
+
+
     }
 });
