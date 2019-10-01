82a4113a640faa55a16d2d5bf7494d825e48146c
claudia <claudia.hazard.14@sansano.usm.cl>
2019-10-01 05:04:17 -0300

    updates

--- Menu-dirigente/screens/cambiar_unidad.js
+++ Menu-dirigente/screens/cambiar_unidad.js
@@ -10,17 +10,29 @@ import {
     TouchableWithoutFeedback,
     Keyboard,
     Alert,
+    StatusBar,
     TouchableOpacity,
     KeyboardAvoidingView,
     ScrollView,
+    Dimensions,
+    SafeAreaView,
     ActivityIndicator,
-    FlatList
+    FlatList,
+    Platform,
+    UIManager
 } from "react-native";
 import { Header,Left,Right,Icon,Body } from 'native-base'
 import {SCLAlert, SCLAlertButton} from 'react-native-scl-alert'
 import { NavigationEvents } from 'react-navigation';
 import { List, ListItem} from "react-native-elements";
-import SearchBar from "react-native-dynamic-search-bar"
+import GradientCard from "react-native-gradient-card-view";
+import SearchBar from "react-native-dynamic-search-bar";
+import { CustomLayoutSpring } from "react-native-animation-layout";
+import { LinearGradient } from 'expo';
+
+
+const { width } = Dimensions.get("window");
+
 class prueba extends Component {
     static navigationOptions = {
         drawerLabel: 'Prueba',
@@ -35,8 +47,15 @@ class prueba extends Component {
       loading: false,
       data: [],
       error: null,
-      nombre_n: '', 
+      nombre_n: '',
+      refreshing: false,
+      isLoading: true,
+      page: 1 
     };
+    if (Platform.OS === "android") {
+        UIManager.setLayoutAnimationEnabledExperimental &&
+          UIManager.setLayoutAnimationEnabledExperimental(true);
+      }
 
     this.arrayholder = [];
   }
@@ -61,6 +80,7 @@ class prueba extends Component {
             })
       .then(res => res.json())
       .then((responseData) => {
+        console.log(responseData)
         this.setState({
           data: responseData.data,
           message: responseData.message,
@@ -82,61 +102,113 @@ class prueba extends Component {
       placeholder="Ingresa nombre del niño o niña..."        
       onChangeText={text => this.makeRemoteRequest(text)}
       value={this.state.value}  
-      onPressCancel={() => {
-        this.filterList("");
-      }}
-      onPress={() => alert("onPress")}         
+        onPressCancel={() => {
+    this.filterList("");
+  }}
+  onPress={() => alert("onPress")}         
     />    
   );  
     };
 
-      renderSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: '86%',
-          backgroundColor: '#CED0CE',
-          marginLeft: '14%',
-        }}
-      />
-    );
-  };
+ 
+    renderItem(item) {
+        return (
+          <GradientCard
+            title={item.name}
+            shadowStyle={{
+              ...Platform.select({
+                ios: {
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 3,
+                    height: 3
+                  },
+                  shadowRadius: 3,
+                  shadowOpacity: 0.4
+                },
+                android: {
+                  elevation: 3
+                }
+              })
+            }}
+            imageSource={item.edad}
+            subtitle={item.user}
+            width={width * 0.9}
+            style={{
+              width: width,
+              marginTop: 16,
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+            centerSubtitleStyle={{
+              fontSize: 12,
+              marginLeft: 8,
+              textAlign: "center",
+              color: item.strokeColor
+            }}
+            rightComponent={
+              <View>
+
+              </View>
+            }
+          />
+        );
+      }
+    
+      onRefresh = () => {
+        this.setState({
+          dataSource: [],
+          isLoading: false,
+          refreshing: true,
+          seed: 1,
+          page: 1
+        });
+        // this.fetchData();
+      };
+    
+      loadMore = () => {
+        this.setState({
+          // refreshing: true,
+          page: this.state.page + 1
+        });
+        // this.fetchData();
+      };
 
   render() {
     return(
-      <View style={{ flex: 1 }}> 
-    <View style={{ flex: 1 }}>
-        <FlatList
-        ItemSeparatorComponent={this.renderSeparator} 
-        ListHeaderComponent={this.renderHeader}  
-        data = {this.state.data}
-        renderItem={({ item }) => (
-            <ListItem
-              title={`${item.nombre}`}
-              containerStyle={{ borderBottomWidth: 0 }} 
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#21283d" }}>
+        <StatusBar barStyle={"light-content"} />
+        <View style={styles.container}>
+          <SearchBar 
+            onPressToFocus
+            autoFocus={false}
+            fontColor="#c6c6c6"
+            iconColor="#c6c6c6"
+            shadowColor="#282828"
+            cancelIconColor="#c6c6c6"
+            backgroundColor="#353d5e"
+            placeholder="Ingresa nombre del niño o niña..."
+            onChangeText={text => {
+              this.makeRemoteRequest(text);
+            }}
+            value={this.state.value} 
+            onPressCancel={() => {
+              this.makeRemoteRequest("");
+            }}
+            onPress={() => alert("onPress")}
+          />
+          <View style={{ top: 12 }}>
+            <FlatList
+              data={this.state.data}
+              renderItem={({ item }) => this.renderItem(item)}
+              onEndReached={this.loadMore}
+              onRefresh={this.onRefresh}
+              refreshing={this.state.refreshing}
+              keyExtractor={item => item.user}  
             />
-          )}
-          keyExtractor={item => item.user}         
-        />
+          </View>
         </View>
-    <View style={{width: '100%', height: '8%',alignItems:'center', justifyContent:'center'}} >
-                    <Button 
-                    onPress = {() => { }}
-                    icon = {
-                        <Icon
-                        name= 'send'
-                        type= 'FontAwesome'
-                        style={{fontSize: 22, color: 'white'}}
-                        //color= '#ffffff'
-                        />
-                    }iconRight
-                    title = "Crear   "
-                    titleStyle = {{fontFamily: 'Roboto', fontSize: 22}}
-                    buttonStyle = {{backgroundColor: '#104F55',justifyContent:'center'}}
-                    />
-                    </View>
-                    </View>
+      </SafeAreaView>
     );
     }
 }
