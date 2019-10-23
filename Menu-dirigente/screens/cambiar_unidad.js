fa1e7979eaa1e8af79bf055807ef68a7a046c163
claudia <claudia.hazard.14@sansano.usm.cl>
2019-10-23 01:31:22 -0300

    Cambiar unidad
    Mostrar cuando no se encuentra nombre, cambio de boton con aviso de estas seguro, hay que arreglarlo por ahora, avisar que el usuario no tiene unidad y que debe crear una

--- Menu-dirigente/screens/cambiar_unidad.js
+++ Menu-dirigente/screens/cambiar_unidad.js
@@ -32,7 +32,8 @@ import { CustomLayoutSpring } from "react-native-animation-layout";
 import { LinearGradient } from 'expo';
 import TouchableScale from 'react-native-touchable-scale';
 import CustomButton from "../CustomComponents/CustomButtons";
-
+import {Alerta2Botones} from './../CustomComponents/customalert copy'
+import {Alerta} from './../CustomComponents/customalert'
 
 const { width } = Dimensions.get("window");
 
@@ -60,11 +61,14 @@ class cambiar_unidad extends Component {
       show1: false,
       show2: false,
       userToken: "",
-      SendAlertType: 1,
-      SendAlertMessage: "",
       press: true,
       mostrarSearchBar: true,
-      cancel1: null
+      cancel1: null,
+      estadoAlerta: false,
+      tituloAlerta: "Cuidado",
+      mensajeAlerta: "Al cambiar de unidad, no podras revertir este cambio.",
+      typeAlerta: 'Warning',
+      estadoAlerta2Botones: false,
       
     };
     if (Platform.OS === "android") {
@@ -91,61 +95,8 @@ class cambiar_unidad extends Component {
     this.setState({isLoading : false})
 
 }
-  ShowSendAlert(){
-
-console.log(this.state.SendAlertType)
-    if (this.state.SendAlertType == 0){
-        return(
-        <ActivityIndicator
-            animating = {this.state.SendAlertState}
-            size="large" 
-            color="#00ff00" 
-        />);
-    }
-    else if(this.state.SendAlertType == 1){
-        return(
-            <SCLAlert
-            theme="success"
-            show={this.state.SendAlertState}
-            title="Felicidades"
-            subtitle= {this.state.SendAlertMessage}
-            onRequestClose = {this.handleClose}
-            >
-            <SCLAlertButton theme="success" onPress={() => {this.handleClose(); this.props.navigation.goBack()}}>Aceptar</SCLAlertButton>
-            </SCLAlert>
-        );
-    }
-    else if(this.state.SendAlertType == -1){
-        return(
-            <SCLAlert
-            theme="danger"
-            show={this.state.SendAlertState}
-            title="Ooops"
-            subtitle= {this.state.SendAlertMessage}
-            onRequestClose = {this.handleClose}
-            >
-            <SCLAlertButton theme="danger" onPress={this.handleClose}>Aceptar</SCLAlertButton>
-            </SCLAlert>
-        );
-    }
-    else{
-        return(
-            <SCLAlert
-            theme="warning"
-            show={this.state.SendAlertState}
-            title="Estoy Confundido"
-            subtitle= {this.state.SendAlertMessage}
-            onRequestClose = {this.handleClose}
-            >
-            <SCLAlertButton theme="warning" onPress={this.handleClose}>Aceptar</SCLAlertButton>
-            </SCLAlert>
-        );
-    }
-
-}
 
 LoadingState(){
-  console.log(this.state.isLoading)
   if(this.state.isLoading){
       return(
 
@@ -271,7 +222,11 @@ LoadingState(){
           data3: responseData.data,
           SendAlertMessage: responseData.message,
           error: null,
-          isLoading:false
+          isLoading:false,
+          typeAlerta: 'Succsess',
+          estadoAlerta: true,
+          tituloAlerta : "Éxito",
+          mensajeAlerta : "La unidad fue cambiada correctamente"
         });
       })
       .catch(error => {
@@ -344,6 +299,7 @@ LoadingState(){
 
       
       selectItem(item) {
+        if(this.state.mostrarSearchBar){
         this.setState({
           value:item.nombre,
           text:item.nombre,
@@ -352,9 +308,21 @@ LoadingState(){
           mostrarSearchBar: false,
           cancel1: 'cancel'
         });
-        this.makeRemoteRequest2(this.text2)
-        // this.fetchData();
-      };
+        this.makeRemoteRequest2(this.text2)}
+        else{
+          this.makeRemoteRequest("")
+          this.setState({
+            value:"",
+            text:"",
+            usuario:"",
+            data: this.state.data,
+            mostrarSearchBar: true,
+            cancel1: null,
+            show1:false
+          });
+          }
+        
+        }
 
                 
       selectItem2(item) { 
@@ -387,7 +355,6 @@ LoadingState(){
 
 show1() {
     if (this.state.show1) {
-      console.log(this.state.data) 
       return (
         
         <View> 
@@ -436,7 +403,8 @@ show2() {
             <View style={styles.bottom}>
                     <View style={{width: '100%', height: '8%',alignItems:'center', justifyContent:'center'}} >
                     <CustomButton
-                    onPress = {() => {this.makeRemoteRequest3(this.state.usuario,this.state.ide)}}
+                    //onPress = {() => {this.makeRemoteRequest3(this.state.usuario,this.state.ide)}}
+                    onPress = {() => {this.toggleAlert2Botones()}}
                 
                     title = "Cambiar"
                     name = 'long-primary-button'
@@ -451,38 +419,78 @@ return null;
   }
 }
 parte_inicial() {
-  
-  if(this.state.mostrarSearchBar){
-    return(
-<SearchBar 
-            onPressToFocus
-            autoFocus={false}
-            fontColor="#ffffff"
-            fontSize={16}
-            iconColor="#ffffff"
-            shadowColor={null}
-            cancelIconComponent={this.charge()}
-            cancelIconColor="#ffffff"
-            backgroundColor="#8B4BC1"
-            placeholder="Ingresa nombre del niño o niña..."
-            onChangeText={text => {
-              this.makeRemoteRequest(text);
-            }}
-            value={this.state.text} 
-            onPressCancel={() => {
-              this.makeRemoteRequest("");
-            }}
+    if (this.state.userToken.unidad1!=0){
+      if(this.state.mostrarSearchBar){
+        return(
+          <View>
+          <Text style={{marginLeft:15,fontSize: 16, marginBottom:15}}>Seleccione niño o niña que desea cambiar de unidad:</Text>
+    <SearchBar 
+                onPressToFocus
+                autoFocus={false}
+                fontColor="#ffffff"
+                fontSize={16}
+                iconColor="#ffffff"
+                shadowColor={null}
+                cancelIconComponent={this.charge()}
+                cancelIconColor="#ffffff"
+                backgroundColor="#8B4BC1"
+                placeholder="Ingresa nombre del niño o niña..."
+                onChangeText={text => {
+                  this.makeRemoteRequest(text);
+                }}
+                value={this.state.text} 
+                onPressCancel={() => {
+                  this.makeRemoteRequest("");
+                }}
 
-            textInputValue={this.state.text}
+                textInputValue={this.state.text}
 
-          />)
+              /></View>)
+      }
+      else{
+    return (<View>
+    <Text style={{marginLeft:15,fontSize: 16, marginBottom:15}}>Seleccione niño o niña que desea cambiar de unidad:</Text>
+    </View>);
+    }
   }
   else{
-return null;
+    return(
+    <View style = {styles.container}>
+    <View style = {{flexDirection : 'row', width:'90%', height:'40%', alignItems:'center',alignSelf:'center'}}>
+    <Text style ={{color:'#d7576b',fontFamily:'Roboto',fontSize:30, textAlign: 'center',marginTop:15,marginBottom:15}}>No formas parte de una unidad, crea una o unete a la de otro dirigente.</Text>
+    </View>
+    <View style = {styles.container}>
+    <View style = {{ width:'90%', height:'40%', alignItems:'center',alignSelf:'center',marginTop:15}}>
+                            <CustomButton
+                                onPress = {()=> this.props.navigation.navigate('CrearUnidad')}
+                                title = "Crear nueva unidad"
+                                name = 'long-secondary-button'
+                    
+                            />
+    
+    <CustomButton
+                            onPress = {()=> this.props.navigation.navigate('Unidad')}
+                            title = "Volver"
+                            name = 'long-primary-button'
+
+                        />
+    </View>
+    </View>
+</View>);
   }
 }
+toggleAlert(){
+  this.setState({
+      estadoAlerta : !this.state.estadoAlerta
+  })
+}
 
-
+toggleAlert2Botones(){
+  console.log("DASFGDGFDDGFDGF")
+  this.setState({
+      estadoAlerta2Botones : !this.state.estadoAlerta2Botones
+  })
+}
 
 //Mostrar simbolo de carga en la lista de nines
 charge(){
@@ -502,6 +510,49 @@ charge2(){
   }
   else{
     return(null)
+  }
+}
+
+se_encuentra_en_busqueda(){
+  console.log(this.state.data)
+  if(this.state.data!=undefined){
+    return(
+      <FlatList
+      data = {this.state.data}
+
+      renderItem={({ item }) => (
+
+          <ListItem
+            rightIcon={{name : this.state.cancel1}}
+            containerStyle = { {width: '93%', alignSelf: 'center',borderRadius:10,marginTop:2}}
+            title={`${item.nombre}`}
+            titleStyle={{ color: '#104F55', fontWeight: 'bold' }}
+            onPress={() => this.selectItem(item)}
+            Component={TouchableScale}
+            friction={90} //
+            tension={100} // 
+            activeScale={0.95} //
+            leftAvatar={{ rounded: true, source: require('../assets/perfil.png') }}
+            linearGradientProps={{
+              colors: ['#f2e6ff', '#F9F4FF'],
+              start: [1.5, 0],
+              end: [0.1, 0],
+            }}
+            subtitleStyle={{ color: '#104F55' }}
+            subtitle={`${item.pseudonimo}`}
+            ViewComponent={LinearGradient}
+
+          />
+      )}
+        keyExtractor={item => item.user} 
+                
+      />
+    )
+  }
+  else{
+    return(
+      <Text style={{marginLeft:15,fontSize: 16, marginTop:5}}>No se encuentran personas con ese nombre.</Text>
+    )
   }
 }
 
@@ -526,44 +577,15 @@ charge2(){
                 <ScrollView >
                 <SafeAreaView style={{ flex: 1}}>
                 
-        <Text style={{marginLeft:15,fontSize: 16, marginBottom:15}}>Seleccione niño o niña que desea cambiar de unidad:</Text>
+        
         <View style={styles.container}>
         
-
-        <View >{this.parte_inicial()}</View>
+        
+        {this.parte_inicial()}
         <View style={{ flex: 1 }}>
         <ScrollView > 
                 
-        <FlatList
-        data = {this.state.data}
-
-        renderItem={({ item }) => (
-
-            <ListItem
-              rightIcon={{name : this.state.cancel1}}
-              containerStyle = { {width: '93%', alignSelf: 'center',borderRadius:10,marginTop:2}}
-              title={`${item.nombre}`}
-              titleStyle={{ color: '#104F55', fontWeight: 'bold' }}
-               onPress={() => this.selectItem(item)}
-              Component={TouchableScale}
-              friction={90} //
-              tension={100} // 
-              activeScale={0.95} //
-               leftAvatar={{ rounded: true, source: require('../assets/perfil.png') }}
-               linearGradientProps={{
-                colors: ['#f2e6ff', '#F9F4FF'],
-                start: [1.5, 0],
-                end: [0.1, 0],
-              }}
-              subtitleStyle={{ color: '#104F55' }}
-              subtitle={`${item.pseudonimo}`}
-               ViewComponent={LinearGradient}
-
-            />
-        )}
-          keyExtractor={item => item.user} 
-                  
-        />
+        {this.se_encuentra_en_busqueda()}
         
             
         
@@ -573,14 +595,17 @@ charge2(){
         <View style={styles.container}>{this.show1()}</View>
       
 
-       <View>
-            {this.ShowSendAlert()}
-        </View>
         </View>
       </SafeAreaView>
       
       </ScrollView>
       <View style={styles.container}>{this.show2()}</View>
+
+            <Alerta2Botones visible = {this.state.estadoAlerta2Botones} type = {this.state.typeAlerta} titulo = {this.state.tituloAlerta} contenido = {this.state.mensajeAlerta} aceptar = {() => this.makeRemoteRequest3(this.state.usuario,this.state.ide)} rechazar = {() => {this.toggleAlert2Botones()}}
+                    />
+                    
+            <Alerta visible = {this.state.estadoAlerta} type = {this.state.typeAlerta} titulo = {this.state.tituloAlerta} contenido = {this.state.mensajeAlerta} rechazar = {() => {this.toggleAlert()}}
+                    />
                 </View>
       
     );
